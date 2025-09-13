0540f3219a23a020e3ee8fe2838375703b2254f0
martinhara-trunojoyo <martinharahap02@gmail.com>
2025-09-13 11:29:47 +0700

    250441100033_ImanuelMartin_Pelcode1

---
+++ 250441100033_Imanuel Martin Harax/pelcod.py
@@ -0,0 +1,43 @@
+print("="*50)
+print("Nama : Martin Harahap")
+print("="*50)
+nama = "Martin Harahap"
+print(nama)
+print(type(nama))
+boolean = True
+print(type(boolean))
+
+penjumlahan = 5+7
+print("Hasilnya Adalah",penjumlahan)
+
+nama_asprak = "Andika Kamal"
+nama_calon_dosen = nama_asprak
+print(nama_calon_dosen)
+
+
+# tipe data number
+
+# tahun_lahir = int(input("Masukkan Tahun Lahir : "))
+# tahun_sekarang = int(input("Masukkan Tahun Sekarang : "))
+# penghitungan_usia = tahun_sekarang - tahun_lahir
+# print(penghitungan_usia)
+
+bilangan_1 = 51
+bilangan_2 = 7
+
+# modulus = bilangan_1 % bilangan_2
+# print(modulus)
+
+# jumlah = int(bilangan)+int(bilangan_2)
+# print(jumlah)
+
+# cek = bilangan > bilangan_2
+# print(cek)
+
+saldo = float(input("Masukkan Saldo Awal :"))
+deposit = float(input("Masukkan Saldo Deposit : "))
+
+saldo += deposit
+print(saldo)
+
+
